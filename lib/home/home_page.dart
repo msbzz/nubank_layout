cf9cfa9b4ac2a5389d9c97009dce07eca3f1716f
Marco <mbarozzi@gmail.com>
2024-08-26 18:28:00 -0300

    widget para appbar HomePagw

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nubank_layout/themes/colors.dart';
+import 'package:nubank_layout/utils/custom_app_bar.dart';
 import 'package:nubank_layout/utils/images_app_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,35 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: const Color(ROXO_NUBANK),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 20,
-            ),            Row(
-              children: [
-                SizedBox(height: 10),
-                ImagensAppBar(path:'user.png'),  
-                SizedBox(width: 260),
-                ImagensAppBar(path:'view.png'), // Exemplo de ícone
-                SizedBox(width: 18),
-                ImagensAppBar(path:'help.png'), // Exemplo de ícone
-                SizedBox(width: 18),
-                ImagensAppBar(path:'email.png'),
-              ],
-            ),
-            SizedBox(height: 5),
-            Text(
-              'Olá, Felipe Santana',
-              style: TextStyle(fontSize: 18, color: Colors.white,),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
-      ),
+      appBar: CustomAppBar(height: 120.0), // Altura ajustável
       body: Center(child: Text('Conteúdo da página')),
     );
   }
---
+++ lib/utils/custom_app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:nubank_layout/utils/images_app_bar.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+
+  CustomAppBar({this.height = 100.0}); // Altura padrão pode ser ajustada
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      color: const Color(0xFF8A05BE), // Exemplo de cor de fundo
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+                   Row(
+              children: [
+                SizedBox(height: 70),
+                ImagensAppBar(path:'user.png'),  
+                SizedBox(width: 260),
+                ImagensAppBar(path:'view.png'), // Exemplo de ícone
+                SizedBox(width: 18),
+                ImagensAppBar(path:'help.png'), // Exemplo de ícone
+                SizedBox(width: 18),
+                ImagensAppBar(path:'email.png'),
+              ],
+            ),
+          SizedBox(height: 2),
+          Text(
+            'Olá, Felipe Santana',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
